90d391cb46a0000a966eb42910bfdf900a8f1af9
wsa <woosa0123@gmail.com>
2023-08-17 01:35:30 +0900

    [feat] : 로그인 기능 (미완성)

--- lib/ViewPage.dart
+++ lib/ViewPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ChattingPage.dart';
 import 'WeatherPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ViewPage extends StatefulWidget {
   const ViewPage({Key? key}) : super(key: key);
@@ -10,6 +11,27 @@ class ViewPage extends StatefulWidget {
 }
 
 class _ViewPageState extends State<ViewPage> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggedUser;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser(){
+    try {
+      final user = _authentication.currentUser;
+      if (user != null) {
+        loggedUser = user;
+        print(loggedUser!.email);
+      }
+    }catch(e){
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return PageView(
