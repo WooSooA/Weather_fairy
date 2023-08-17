bac4d68518d77819fba18e8c41e5967400ef4dee
ji ye lee <sophie437653@gmail.com>
2023-08-17 21:49:36 +0900

    first cityl commit

---
+++ lib/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class tests extends StatelessWidget {
+  const tests({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
